b63b6f1620dd635bd3503b51349fbaea8e441cd2
Javier Vilchez <vilchezjavierf@gmail.com>
2024-05-17 18:39:42 -0300

    change typography and add tabs

---
+++ photo-slap/src/app/pages/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [
+    IonIcon,
+    IonTabButton,
+    IonTabBar,
+    IonTabs,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
+})
+export class HomePage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
